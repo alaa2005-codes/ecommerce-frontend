67b634ff4f623e0524761311b16d363a7ab621f1
Alaa Ahmad <0987654321aznd@gmail.com>
2026-06-24 23:49:22 +0300

    تعديل baseURL وإصلاح مشكلة الربط

--- src/Api/baseURL.js
+++ src/Api/baseURL.js
@@ -1,3 +1,7 @@
 import axios from 'axios';
-const baseUrl = axios.create({ baseURL: "https://nodejs-ecommerce-api-v1-production-a56b.up.railway.app" });
+
+const baseUrl = axios.create({ 
+  baseURL: "https://nodejs-ecommerce-api-v1-production-a56b.up.railway.app/api/v1"
+});
+
 export default baseUrl;
